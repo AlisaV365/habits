45de4dc507f3bf9596aac2f901d8a91c633f32b5
AlisaV365 <127328313+AlisaV365@users.noreply.github.com>
2023-09-10 10:02:50 +0300

    доработки

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -1,12 +1,17 @@
 from telebot import TeleBot
 from celery import shared_task
+from goodhabits.models import Habit
+
+bot = TeleBot('6686791128:AAHxQRTi36OKijSyzMACSAHu4hI_jGjFQY8')
 
 @shared_task
 def habits_notification(object_pk):
-   # Получите привычку на основе object_pk
-   habit = получить_привычку(object_pk)
-   habit_act, habit_time, habit_place = получить_детали_привычки(habit)
+    # Получите привычку на основе object_pk
+    habit = Habit.objects.get(pk=object_pk)
 
-   bot = TeleBot('6686791128:AAHxQRTi36OKijSyzMACSAHu4hI_jGjFQY8')
-   message = f'Трекер привычек напоминает: требуется совершить {habit_act} в {habit_time} в {habit_place}'
-   bot.send_message(habit.creator.chat_id, message)
+    habit_act = habit.act
+    habit_time = habit.time
+    habit_place = habit.place
+
+    message = f'Трекер привычек напоминает: требуется совершить {habit_act} в {habit_time} в {habit_place}'
+    bot.send_message(habit.creator.chat_id, message)
